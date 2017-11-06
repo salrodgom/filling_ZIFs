c271194b1d6d640de53902c5890f24ef45a065c4
Salvador Rodríguez-Gómez <salrodgom@upo.es>
2017-11-06 19:24:07 +0100

    modified:   src/lammpstrj2pdb.f90

--- src/lammpstrj2pdb.f90
+++ src/lammpstrj2pdb.f90
@@ -267,25 +267,6 @@ subroutine inverse(a,c,n)
 END SUBROUTINE inverse
 !
 real function volume(rv)
-!
-!  Conditions of use:
-!
-!  GULP is available free of charge to academic institutions
-!  and non-commerical establishments only. Copies should be
-!  obtained from the author only and should not be distributed
-!  in any form by the user to a third party without the express
-!  permission of the author. This notice applies to all parts
-!  of the program, except any library routines which are
-!  distributed with the code for completeness. All rights for
-!  such routines remain with the original distributor.
-!
-!  No claim is made that this program is free from errors and
-!  no liability will be accepted for any loss or damage that
-!  may result. The user is responsible for checking the validity
-!  of their results.
-!
-!  Copyright Curtin University 2004
-!
   implicit none
   real, intent(in)  :: rv(3,3)
   real       :: r1x
