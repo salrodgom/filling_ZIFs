e54c1e04e277734bae4f3c6a7972fe0ec8f0b73f
Salvador Rodríguez-Gómez <salrodgom@upo.es>
2017-11-10 19:43:57 +0100

    modified:   ZIF_structures_Compendium/SOD_ZIF_Imidazol_VASP_222.cif 	deleted:    lib/fff_raspa/DMSO_rigid.def 	modified:   lib/fff_raspa/INPUT 	modified:   main.sh

--- main.sh
+++ main.sh
@@ -4,6 +4,11 @@ nCPU=$2
 n_cycles=5
 temperature=85.0
 pressure=0.0
+filling_mode="RASPA"
+# MC-Filling:
+CyclesEvery=7500
+InitCycles=$(echo "$CyclesEvery * 0.1" | bc -l | sed 's/\./ /g' | awk '{print $1}')
+MoviesEvery=$((CyclesEvery - 1))
 #
 structure=$(echo $CIFFile | sed 's/\.cif//g')
 seed=$(od --read-bytes=3 --address-radix=n --format=uL /dev/urandom | tr --delete " ")
@@ -32,26 +37,101 @@ function update_name {
  NewNameFile=${structure}_${guest}_${n_Ar}
  CyclesNameFile=${cycle_name}_${NewNameFile}_${seed}
 }
+function check_supercell {
+# Check cell size for correct calculation of energies considering cutoff
+ cutoff=16.0
+ ua=1
+ ub=1
+ uc=1
+ a_cell=$(grep "_cell_length_a" $CIFTemporallyFile | awk '{print $2}')
+ b_cell=$(grep "_cell_length_b" $CIFTemporallyFile | awk '{print $2}')
+ c_cell=$(grep "_cell_length_c" $CIFTemporallyFile | awk '{print $2}')
+ a_cell=$(echo "$ua*$a_cell" | bc -l)
+ while [ $(echo "$a_cell < 2*$cutoff" | bc -l) == 1 ] ; do
+  let "ua++"
+  a_cell=$(echo "$ua*$a_cell" | bc -l)
+ done
+ while [ $(echo "$b_cell < 2*$cutoff" | bc -l) == 1 ] ; do
+  let "ub++"
+  b_cell=$(echo "$ub*$b_cell" | bc -l)
+ done
+ while [ $(echo "$c_cell < 2*$cutoff" | bc -l) == 1 ] ; do
+  let "uc++"
+  c_cell=$(echo "$uc*$c_cell" | bc -l)
+ done
+ echo "Make Supercell: ${ua}x${ub}x${uc} > $a_cell $b_cell $c_cell"
+}
+function makeCIFTopology {
+ ./cif2lammps -c ${CIFTemporallyFile} -wq -S
+ mv ${structure}_${seed}_topol.cif ${CIFTemporallyFile}
+ rm ${structure}_${seed}.gin ${structure}_${seed}.pdb ${structure}_${seed}.data
+}
+function mc_muVT_raspa {
+ # mu V T, ensemble
+ # RASPA
+ cp ${raspa_files_folder}/*.def .
+ cp ${raspa_files_folder}/INPUT .
+ cp ${CIFTemporallyFile} .
+ cp ../cif2lammps .
+ cp ../lib/forcefield.lib .
+ sed "s/STRUCTURE/${CIFTemporallyFile}/g" INPUT > simulation.input
+ sed -i "s/RANDOMSEED/${seed}/g"         simulation.input
+ sed -i "s/TEMPERATURE/${temperature}/g" simulation.input
+ sed -i "s/PRESSURE/${pressure}/g" simulation.input
+ sed -i "s/GUEST/${guest}/g" simulation.input
+ sed -i "s/CYCLESEVERY/${CyclesEvery}/g" simulation.input
+ sed -i "s/INITCYCLES/${InitCycles}/g" simulation.input
+ sed -i "s/MOVIESEVERY/${MoviesEvery}/g" simulation.input
+ #check_supercell
+ ua=1
+ ub=1
+ uc=1
+ #
+ sed -i "s/SUPERCELL/$ua $ub $uc/g" simulation.input
+ makeCIFTopology
+ go_raspa
+ #wait_for_raspa
+ sed '/MODEL    2/,$d' Movies/System_0/Movie*allcomponents.pdb > c
+ sed 's/MODEL    2/MODEL    1/g' c > input.pdb
+ rm c
+ rm -rf VTK Movies/System_0/Framework* Movies/System_0/Movie_*_component_*.pdb Movies/System_0/Movie_*_frameworks.pdb
+ ./pdb2cif
+ mv p1.cif ${CIFTemporallyFile}
+ cp ${CIFTemporallyFile} ../${CIFTemporallyFile}
+ cp ${CIFTemporallyFile} ../${CyclesNameFile}.cif
+}
 function fill_with_guest {
  update_name
  folder=${CyclesNameFile}_filling
  if [ ! -d $folder ] ; then
   mkdir $folder
-  cd $folder
-   cp ../${CIFTemporallyFile} .
-   cp ../adsorption_fast_atom_saturation_01 .
-   ./adsorption_fast_atom_saturation_01 ${CIFTemporallyFile} > out.txt
-   n_Ar=$(grep 'Ar atoms' out.txt | awk '{print $3}')
-   Arname=$(echo $CIFTemporallyFile | sed 's/\.cif//g')_Ar.cif
-   update_name
-   cp ${Arname} ../${CIFTemporallyFile}
-   cp ${Arname} ../${CyclesNameFile}.cif
-  cd ..
+  case "${filling_mode}" in
+   Rabdel_Code)
+    cd $folder
+     cp ../${CIFTemporallyFile} .
+     cp ../adsorption_fast_atom_saturation_01 .
+     ./adsorption_fast_atom_saturation_01 ${CIFTemporallyFile} > out.txt
+     n_Ar=$(grep 'Ar atoms' out.txt | awk '{print $3}')
+     Arname=$(echo $CIFTemporallyFile | sed 's/\.cif//g')_Ar.cif
+     update_name
+     cp ${Arname} ../${CIFTemporallyFile}
+     cp ${Arname} ../${CyclesNameFile}.cif
+    cd ..
+    ;;
+   RASPA)
+    cd $folder
+     mc_muVT_raspa
+    cd ..
+   *) echo "Invalid option"
+      exit 0
+    ;;
+  esac
  fi
 }
 function interface_adsorption_lammps {
  lammps_file_lib="in.lmp"
  cp lib/forcefield.lib .
+ cp ../lib/forcefield.lib .
  flags="-S"
  if [ "${flags_cif2lammps}" == "post-loading" ] ; then
   flags="-f -wq -S"
@@ -93,6 +173,15 @@ function em_md_lammps {
   cd ..
  fi
 }
+function go_raspa {
+ count_used_CPUs
+ while [ $(echo "${n_used} >= ${nCPU}" | bc -l) == 1 ] ; do
+  sleep 30
+  count_used_CPUs
+ done
+ raspa
+ sleep 1
+}
 function go_lammps {
  count_used_CPUs
  while [ $(echo "${n_used} >= ${nCPU}" | bc -l) == 1 ] ; do
@@ -114,6 +203,11 @@ function lammps {
  time > salida.lammps
  nohup mpirun --np ${nCPU} ~/lammps/src/lmp_fftw -in in.lmp -sf opt >> salida.lammps 
  time >>  salida.lammps
+}
+function raspa {
+ time > salida.raspa
+ nohup simulate >> salida.raspa
+ time >> salida.raspa
 }
 function lammps_raspa {
  cp ../lammpstrj2pdb .
@@ -184,7 +278,7 @@ first_optimisation
 distance_angle_measure
 energy0=$(tail -n1 ${CyclesNameFile}_emmd/logs/minimization_postMD.txt | awk '{print $5}')
 for i in $(seq 1 ${n_cycles}) ; do
- guest='Ar'
+ guest='argon'
  let cycle++
  cycle_name=$(echo $cycle | awk '{ printf("%02d\n", $1) }')
  fill_with_guest
@@ -197,11 +291,10 @@ for i in $(seq 1 ${n_cycles}) ; do
  distance_angle_measure
  resumen
  previous_name=${CyclesNameFile}
- old_guest=${guest}
- guest='Xe'
+ guest='xenon'
  cycle_name=$(echo $cycle | awk '{ printf("%02d\n", $1) }')
  update_name
- sed "s/${old_guest} /${guest} /g" ${previous_name}.cif > ${CyclesNameFile}.cif
+ sed "s/Ar /Xe /g" ${previous_name}.cif > ${CyclesNameFile}.cif
  remove_guest="true"
  flags_cif2lammps="post-Xe-Ar-exchange"
  interface_adsorption_lammps
